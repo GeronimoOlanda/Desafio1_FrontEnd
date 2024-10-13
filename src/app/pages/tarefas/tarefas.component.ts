c60be787fd6348dffab74dbd6bffec0231896b66
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-10-12 22:15:44 -0300

    versão 2.3

--- src/app/pages/tarefas/tarefas.component.ts
+++ src/app/pages/tarefas/tarefas.component.ts
@@ -4,7 +4,6 @@ import { Tarefa } from '../../models/tarefa';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-tarefas',
   standalone: true,
@@ -16,7 +15,7 @@ export class TarefasComponent implements OnInit {
   tarefas: Tarefa[] = [];
   page = 1;
   pageSize = 10;
-  newTarefa: Tarefa = { id: 0, titulo: '', descricao: '', concluida: false, usuarioId: 0 }; // Aqui
+  newTarefa: Tarefa = { id: 0, titulo: '', descricao: '', concluida: false, usuarioId: 0 };
 
   constructor(private tarefasService: TarefasService) { }
 
@@ -31,7 +30,7 @@ export class TarefasComponent implements OnInit {
   }
 
   addTarefa(): void {
-    this.newTarefa.usuarioId = 1; // Defina o ID do usuário autenticado aqui (ou de outra forma, se você tiver um serviço de autenticação)
+    this.newTarefa.usuarioId = 1; // Defina o ID do usuário autenticado aqui
     this.tarefasService.addTarefa(this.newTarefa).subscribe(() => {
       this.loadTarefas();
       this.newTarefa = { id: 0, titulo: '', descricao: '', concluida: false, usuarioId: 0 }; // Reseta os campos
@@ -40,12 +39,28 @@ export class TarefasComponent implements OnInit {
 
   toggleConcluida(tarefa: Tarefa): void {
     tarefa.concluida = !tarefa.concluida;
-    this.tarefasService.updateTarefa(tarefa).subscribe();
+    this.tarefasService.updateTarefa(tarefa).subscribe(() => {
+      this.loadTarefas();
+    });
   }
 
   deleteTarefa(id: number): void {
     this.tarefasService.deleteTarefa(id).subscribe(() => {
       this.loadTarefas();
     });
+  }
+
+  // Função para ir para a página anterior
+  previousPage(): void {
+    if (this.page > 1) {
+      this.page--;
+      this.loadTarefas();
+    }
+  }
+
+  // Função para ir para a próxima página
+  nextPage(): void {
+    this.page++;
+    this.loadTarefas();
   }
 }
